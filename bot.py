3d2ef2ca0de3d80ae442d24de7a5b25922192713
snectrisadas <288873933+snectrisadas@users.noreply.github.com>
2026-07-30 12:48:46 -0300

    Remove comentarios do codigo e ignora a pasta .vscode

--- bot.py
+++ bot.py
@@ -1,62 +1,66 @@
-import discord                        
-import os                             
-import sqlite3 
-import logging                       
-from dotenv import load_dotenv        
-from discord.ext import commands      
-load_dotenv()                         
+import discord
+import os
+import sqlite3
+import logging
+from dotenv import load_dotenv
+from discord.ext import commands
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")   
+load_dotenv()
 
-autorizacoes = discord.Intents.default()     
-autorizacoes.message_content = True         
-autorizacoes.members = True                  
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+autorizacoes = discord.Intents.default()
+autorizacoes.message_content = True
+autorizacoes.members = True
 
 bot = commands.Bot(command_prefix="$", intents=autorizacoes)
-apelidos = ["mega", "trontron","meguinha","megatron", "tron","palhaçao"] 
+apelidos = ["mega", "trontron", "meguinha", "megatron", "tron", "palhaçao"]
 
-logging.basicConfig(filename="diario.log", level=logging.INFO)  
-   
+logging.basicConfig(filename="diario.log", level=logging.INFO)
 
-banco = sqlite3.connect("tarefas.db") 
-cursor = banco.cursor()  # cria o cursor (a "caneta") que executa os comandos SQL dentro do banco
+banco = sqlite3.connect("tarefas.db")
+cursor = banco.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS tarefas (descricao TEXT)")
 
-@bot.event                            
-async def on_message(msg):            
 
-    if msg.author == bot.user:        # se quem mandou a mensagem foi o próprio bot...
-        return                        # ...para aqui (pra ele não responder a si mesmo)
+@bot.event
+async def on_message(msg):
+    if msg.author == bot.user:
+        return
 
-    texto = msg.content.lower()       
-    palavras = texto.split()          
-    for item in palavras:             
-        if item in apelidos:          
-            await msg.channel.send("MEGATRON? ONDE? QUEM? *procura por si mesmo* AH, SOU EU! HAHAHAHA!")  # responde no
-            break                     
+    texto = msg.content.lower()
+    palavras = texto.split()
+    for item in palavras:
+        if item in apelidos:
+            await msg.channel.send("MEGATRON? ONDE? QUEM? *procura por si mesmo* AH, SOU EU! HAHAHAHA!")
+            break
 
-    await bot.process_commands(msg)   
-@bot.command()                        
-async def risos(ctx):   
-    logging.info(f"{ctx.author} usou o comando risos")              
-    await ctx.send("A desenvolvedora nº 197-0i00 me pediu pra rir uma última vez antes de... enfim. HAHAHAHAHA. Em memória dela.")  
+    await bot.process_commands(msg)
 
-@bot.command()                        
-async def tarefa(ctx,*,descricao):    # $tarefa; o *, faz "descricao" pegar a frase TODA (com espaços)
-    cursor.execute("INSERT INTO tarefas (descricao) VALUES (?)", (descricao,))   # insere a tarefa na tabela
-    banco.commit()                    # confirma e grava no disco de verdade
+
+@bot.command()
+async def risos(ctx):
+    logging.info(f"{ctx.author} usou o comando risos")
+    await ctx.send("A desenvolvedora nº 197-0i00 me pediu pra rir uma última vez antes de... enfim. HAHAHAHAHA. Em memória dela.")
+
+
+@bot.command()
+async def tarefa(ctx, *, descricao):
+    cursor.execute("INSERT INTO tarefas (descricao) VALUES (?)", (descricao,))
+    banco.commit()
     logging.info(f"{ctx.author} usou o comando tarefa")
     await ctx.send(descricao)
 
-@bot.command()                        
-async def tarefas(ctx):  
-    
-    cursor .execute("SELECT descricao FROM tarefas")
-    resultado = cursor.fetchall()   
-    resposta = ""
-    for linha in resultado:     
-            resposta = resposta + "- " + linha[0] + "\n" 
-    logging.info(f"{ctx.author} usou o comando tarefas")
-    await ctx.send(resposta)     
 
-bot.run(DISCORD_TOKEN)                
+@bot.command()
+async def tarefas(ctx):
+    cursor.execute("SELECT descricao FROM tarefas")
+    resultado = cursor.fetchall()
+    resposta = ""
+    for linha in resultado:
+        resposta = resposta + "- " + linha[0] + "\n"
+    logging.info(f"{ctx.author} usou o comando tarefas")
+    await ctx.send(resposta)
+
+
+bot.run(DISCORD_TOKEN)
